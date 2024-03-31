4a1a0095337f0f9c320da2a9a7796f3c5c9fcbe3
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-03-31 11:18:25 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, { useState,useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [search, setSearch] = useState("");
+  const [filtered, setFiltered] = useState(fruits);
+
+  useEffect(() => {
+    setFiltered(fruits.filter((fruit) => {
+      return fruit.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    }))
+  },[search])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" onChange={(event) => setSearch(event.target.value)} />
+      <ul>
+        {
+          filtered.map((fruit) => {
+            return <li>{fruit}</li>
+          })
+        }
+      </ul>
+
     </div>
   )
 }
